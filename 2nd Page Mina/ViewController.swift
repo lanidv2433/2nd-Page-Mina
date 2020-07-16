278592f3d2a348cf660f1b5f906c7bc1b9ed3a88
lanidv2433 <lanidv2433@gmail.com>
2020-07-16 11:49:18 -0400

    2nd page w/ buttons

--- 2nd Page Mina/ViewController.swift
+++ 2nd Page Mina/ViewController.swift
@@ -10,11 +10,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var affirmationsButton: UIButton!
+    
+    @IBOutlet weak var journalButton: UIButton!
+    
+    @IBOutlet weak var waterReminder: UIButton!
+    
+    @IBOutlet weak var breaksButton: UIButton!
+    
+    @IBOutlet weak var aboutButton: UIButton!
+    
+    
+    @IBOutlet weak var arrowButton: UIButton!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        affirmationsButton.backgroundColor = UIColor.init(red : 235/255, green : 115/255, blue : 252/255, alpha : 1)
+        affirmationsButton.layer.cornerRadius = 25.0
+        affirmationsButton.tintColor = UIColor.white
+        
+        journalButton.backgroundColor = UIColor.init(red : 235/255, green : 115/255, blue : 252/255, alpha : 1)
+        journalButton.layer.cornerRadius = 25.0
+        journalButton.tintColor = UIColor.white
+        
+        waterReminder.backgroundColor = UIColor.init(red : 235/255, green : 115/255, blue : 252/255, alpha : 1)
+        waterReminder.layer.cornerRadius = 25.0
+        waterReminder.tintColor = UIColor.white
+        
+        breaksButton.backgroundColor = UIColor.init(red : 235/255, green : 115/255, blue : 252/255, alpha : 1)
+        breaksButton.layer.cornerRadius = 25.0
+        breaksButton.tintColor = UIColor.white
+        
+        aboutButton.backgroundColor = UIColor.init(red : 235/255, green : 115/255, blue : 252/255, alpha : 1)
+        aboutButton.layer.cornerRadius = 25.0
+        aboutButton.tintColor = UIColor.white
+        
+        
+    
+        
+        
+        
+        
+        
     }
+    
+       
+       }
 
 
-}
+   
+    
+    
+    
+    
+    
+    
+
 
